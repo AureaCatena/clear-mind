1f8cd6d747691201062ef2a52c9752f613d135a3
sade-yakubova <sade.yakubova@gmail.com>
2025-09-13 16:39:28 +0200

    Finished Home page completely, + footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import Therapists from './pages/Therapists'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -25,6 +26,7 @@ const App = () => {
         <Route path='/therapists' element={<Therapists />} />
         <Route path='/therapists/:speciality' element={<Therapists />} />
       </Routes>
+      <Footer/> 
     </div>
   )
 }
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            {/* ---- Left section ---- */}
+            <div>
+                <img className='mb-5 w-40' src={assets.logo} alt="" />
+                <p className='w-full md:w-2/3 text-grey-600 leading-6'>“Until you make the unconscious conscious, it will direct your life and you will call it fate.” – C.G. Jung</p>
+            </div>
+
+            {/* ---- Center section ----- */}
+            <div>
+                <p className='rext-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-2 text-gray-600'>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact us</li>
+                    <li>Privacy policy</li>
+                </ul>
+            </div>
+
+            {/* ---- Right section ----- */}
+            <div>
+                <p className='rext-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul  className='flex flex-col gap-2 text-gray-600'>
+                    <li>+123 456 7890</li>
+                    <li>clearmind.gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <div>
+        {/* ---- Copyright text ----- */}
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2024@ GreatStack - All Rights Resereved</p>
+        </div>
+    </div>
+    
+  )
+}
+
+export default Footer
