19895158586c2eae42cd6520d3bf9cc6e0c1f807
sittora <sittora@gmail.com>
2025-09-14 15:57:56 -0400

    started workin on therapist info on appointment page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
+import { assets } from '../assets/assets'
 
 const Appointment = () => {
 
@@ -20,9 +21,34 @@ const Appointment = () => {
     fetchTherapistInfo()
   }, [therapists, therId])
 
-  return (
+  return therapistInfo && (
     <div>
-        
+      {/* -------------- Therapist Details ------------*/} 
+      <div className='flex flex-col sm:flex-row gap-4'>
+        <div>
+          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={therapistInfo.image} alt="" />
+        </div>
+
+        <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
+          {/* -------------- Therapist Info: name, degree, experience ------------*/} 
+          <p>
+            {therapistInfo.name} 
+            <img src={assets.verified_icon} alt="" />
+          </p>
+          <div>
+           <p>{therapistInfo.degree} - {therapistInfo.speciality}</p>
+           <button>{therapistInfo.experience}</button>
+          </div>
+
+        {/* -------------- Therapist About------------*/} 
+        <div>
+         <p>
+          About <img src={assets.info_icon} alt="" /> 
+        </p>
+         <p>{therapistInfo.about}</p>
+        </div>
+        </div>
+      </div> 
     </div>
   )
 }
